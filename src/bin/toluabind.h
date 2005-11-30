b4caf95ec2930a913c6ea94d48f813772b2e60b8
Ariel Manzur <puntob@gmail.com>
2005-11-30 22:38:28 +0000

    adding to berlios

---
+++ src/bin/toluabind.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: tolua
+** Generated automatically by tolua++-1.0.7 on Wed Nov 30 17:58:04 2005.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
+
