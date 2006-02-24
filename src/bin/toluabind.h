6e8ef6fb04b2cfbda2c818fc14d6f24097d789b6
Ariel Manzur <puntob@gmail.com>
2006-02-24 17:12:39 +0000

    small bug and doc changes

--- src/bin/toluabind.h
+++ src/bin/toluabind.h
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: tolua
-** Generated automatically by tolua++-1.0.8pre4 on Sun Feb 12 20:30:03 2006.
+** Generated automatically by tolua++-1.0.90 on Sun Feb 19 00:38:12 2006.
 */
 
 /* Exported function */
