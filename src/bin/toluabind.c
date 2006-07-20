523f4cfbda0f1d7835aaa457dff5c026f6f62b41
Ariel Manzur <puntob@gmail.com>
2006-07-20 00:35:31 +0000

    probably nothing

--- src/bin/toluabind.c
+++ src/bin/toluabind.c
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: tolua
-** Generated automatically by tolua++-1.0.92 on Fri Jun 23 02:25:33 2006.
+** Generated automatically by tolua++-1.0.92 on Fri Jul  7 23:30:20 2006.
 */
 
 #ifndef __cplusplus
